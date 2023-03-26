ba3cc93bc2dad154d1a5ab6ca6df04c6c2808b0d
ColinSuth <colintgsutherland@gmail.com>
2023-03-25 21:01:03 -0400

    Final
    Done apod_desktop.py

--- apod_api.py
+++ apod_api.py
@@ -1,12 +1,17 @@
 import requests
+
 '''
 Library for interacting with NASA's Astronomy Picture of the Day API.
 '''
-APOD_URL = f'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=1995-06-16&end_date&thumbs=True'
-resp_msg = requests.get(APOD_URL)
+APOD_URL = 'https://api.nasa.gov/planetary/apod'
+APOD_KEY = 'fkWhvO6Ae3R0wHgghBuomv4Fl84FfPJi7GV3fKlD'
 
 def main():
     # TODO: Add code to test the functions in this module
+
+    info = get_apod_info('1999-02-22')
+    url = get_apod_image_url(info)
+    print(url)
     return
 
 def get_apod_info(apod_date):
@@ -19,7 +24,12 @@ def get_apod_info(apod_date):
     Returns:
         dict: Dictionary of APOD info, if successful. None if unsuccessful
     """
-    return   
+    pic_url = f'{APOD_URL}?api_key={APOD_KEY}&date={apod_date}&thumbs=True'
+    resp_msg = requests.get(pic_url)
+    if resp_msg.ok:
+        diction = resp_msg.json()
+        return diction
+    return   None
 
 def get_apod_image_url(apod_info_dict):
     """Gets the URL of the APOD image from the dictionary of APOD information.
@@ -33,7 +43,12 @@ def get_apod_image_url(apod_info_dict):
     Returns:
         str: APOD image URL
     """
-    return
+    if 'thumbnail_url' in apod_info_dict:
+        thumbs = apod_info_dict['thumbnail_url']
+        return thumbs
+    else:
+        hdurl = apod_info_dict['hdurl']
+        return hdurl
 
 if __name__ == '__main__':
     main()
